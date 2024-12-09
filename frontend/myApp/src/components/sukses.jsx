cdefee884da8c75604772e27bc21a9b6658136d9
Akhfa Riksaraga Utama <145185015+AkhfaRU@users.noreply.github.com>
2024-12-09 20:00:04 +0700

    Create sukses.jsx

---
+++ frontend/myApp/src/components/sukses.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Sukses() {
+  return (
+    <div>
+      <h1>Terima Kasih</h1>
+      <p>Pendaftaran Telah Berhasil</p>
+    </div>
+  );
+}
+
+export default Sukses;
