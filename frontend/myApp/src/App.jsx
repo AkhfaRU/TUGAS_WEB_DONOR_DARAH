b9bfa4e1df2fcee9c7b8184064ea9c097ed5c9a7
Akhfa Riksaraga Utama <145185015+AkhfaRU@users.noreply.github.com>
2024-12-09 20:10:48 +0700

    Update App.jsx

--- frontend/myApp/src/App.jsx
+++ frontend/myApp/src/App.jsx
@@ -4,32 +4,68 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>TESTING YA BANG</div>
+    // <div className="container">
+    //   <h1 className='form'>REGISTRASI DONOR DARAH</h1><br />
+    //   <form>
+    //     <label htmlFor="Nama" aria-required>Nama</label>
+    //     <input type="text" placeholder='Nama Lengkap' name='Nama'/>
 
+    //     <label htmlFor="ttl">Tempat Tanggal Lahir</label>
+    //     <input type="text" placeholder='Bandung, 03 Juli 2005' name='ttl'/>
+
+    //     <label htmlFor="ktp">KTP</label>
+    //     <input type="text" placeholder='Masukan 16 Digit Angka' />
+
+    //     <label htmlFor="job">Pekerjaan</label>
+    //     <select name="job" id="job">
+    //       <option selected disabled>--Pilih--</option>
+    //       <option value="wiraswasta">Wiraswasta</option>
+    //       <option value="tni">TNI</option>
+    //       <option value="dokter">Dokter</option>
+    //       <option value="guru">Guru</option>
+    //       <option value="irt">Ibu Rumah Tangga</option>
+    //       <option value="not">Tidak Bekerja</option>
+    //     </select>
+
+    //     <label htmlFor="subjek">Jenis kelamin</label>
+    //     <select name="subjek" id="subjek">
+    //       <option selected disabled>--Pilih--</option>
+    //       <option value="laki-laki">Laki-laki</option>
+    //       <option value="perempaun">perempaun</option>
+    //     </select>
+
+    //     <label htmlFor="no">No WhatsApp / Telephone</label>
+    //     <input type="text" placeholder='08123456789' name='no'/>
+
+    //     <label htmlFor="subjek">Golongan Darah</label>
+    //     <select className="subjek" id="subjek">
+    //       <option selected disabled>--Pilih--</option>
+    //       <option value="A">A</option>
+    //       <option value="B">B</option>
+    //       <option value="AB">AB</option>
+    //       <option value="O">O</option>
+    //     </select>
+
+    //     <label htmlFor="subjek">Riwayat Penyakit</label>
+    //     <select className="subjek" id="subjek">
+    //       <option selected disabled>--Pilih--</option>
+    //       <option value="kanker">Kanker</option>
+    //       <option value="jantung">Jantung</option>
+    //       <option value="hiv/aids">HIV/AIDS</option>
+    //       <option value="opsional">Tidak Memiliki Riwayat Penyakit</option>
+    //     </select>
+    //       <small class="form-note"><strong>Note : </strong>Jika anda tidak memiliki penyakit yang tertera maka pilih opsi kosong.</small><br />
+
+    //       <label htmlFor="tanggal">Tanggal Rencana Donor:</label>
+    //       <input className='date' type="date"/>
+
+    //     <label htmlFor="subjek">Alamat</label>
+    //     <textarea className="alamat" id="alamat" placeholder='Masukan Alamat Lengkap Anda'></textarea>
+    //     <button type='submit'>Submit</button>
+    //   </form>
+    // </div>
+  );
+}
 export default App
